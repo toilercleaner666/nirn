9fef10290205f4b3aa35a7dc895ccd50c40fa533
Anurag-Shah <anuragshah2000@gmail.com>
2020-12-12 18:50:10 +0530

    Fixed province name bug and structural changes to the repo

--- NirnUniversalis/history/provinces/copy.sh
+++ NirnUniversalis/history/provinces/copy.sh
@@ -1,10 +1,10 @@
-##!/bin/sh
-#for i in {1..4272}
-#do
-#	cat ${i}-${i}.txt
-#	cat TEMPLATE.txt > ${i}-${i}.txt
-#done
+#!/bin/sh
 for i in {1..4272}
 do
-	sed -i "s/ - /${i}-${i}/" ${i}-${i}.txt
+	cat ${i}-${i}.txt
+	cat TEMPLATE.txt > ${i}-${i}.txt
 done
+#for i in {1..4272}
+#do
+#	sed -i "s/ - /${i}-${i}/" ${i}-${i}.txt
+#done
